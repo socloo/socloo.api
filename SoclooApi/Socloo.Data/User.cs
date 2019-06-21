1f31452ba8f4cfb87e158caeb9c9b462c875d1dd
Niccolò Andreas Armellini <niccolo.armellini@live.com>
2019-06-21 09:00:13 +0200

    fix

--- SoclooApi/Socloo.Data/User.cs
+++ SoclooApi/Socloo.Data/User.cs
@@ -2,9 +2,28 @@
 using System.Collections.Generic;
 using System.Text;
 
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
 namespace Socloo.Data
 {
     class User
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("Users")]
+        public string FullName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Bio { get; set; }
+
+
+        /*        Login Data
+        Profile Pic
+
+        */
+
     }
 }
