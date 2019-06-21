80583ddf9988b2afd016ed07449c8f2bd37c4a7d
Niccolò Andreas Armellini <niccolo.armellini@live.com>
2019-06-21 13:50:11 +0200

    fix

---
+++ SoclooApi/Socloo.Api/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Socloo.;
+
+
+namespace Socloo.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly MongoDbContext
+    }
+}
