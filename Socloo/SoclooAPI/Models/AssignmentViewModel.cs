4bc4c00b845569162f1f19c72cdfc45d00e0c9c1
Niccolò Andreas Armellini <niccolo.armellini@live.com>
2019-06-28 15:00:48 +0200

    fix

---
+++ Socloo/SoclooAPI/Models/AssignmentViewModel.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace SoclooAPI.Models
+{
+    public class AssignmentViewModel
+    {
+       
+        public ObjectId Id { get; set; }
+
+        [BsonRepresentation(BsonType.ObjectId)]
+        public List<string> TeachersId { get; set; }
+        [BsonRepresentation(BsonType.ObjectId)]
+        public List<string> StudentsId { get; set; }
+        [BsonRepresentation(BsonType.DateTime)]
+        public string ExpirationDate { get; set; }
+        public string Info { get; set; }
+        public string FileId { get; set; }
+    }
+}
