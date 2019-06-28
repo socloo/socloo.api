1bcfb5067811dcb0eca926766c915feb0c41c64d
Niccolò Andreas Armellini <niccolo.armellini@live.com>
2019-06-28 17:37:31 +0200

    fix

---
+++ Socloo/SoclooAPI/Controllers/FilesController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using Microsoft.AspNetCore.Mvc;
+using SoclooAPI.Models;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using Newtonsoft.Json;
+using Nancy.Json;
+using MongoDB.Bson.IO;
+using MongoDB.Driver.GridFS;
+
+namespace SoclooAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+
+        private MongoDBContext mongoDB;
+        public FilesController()
+        {
+            mongoDB = new MongoDBContext();
+
+        }
+
+
+        [HttpPost]
+        public bool UploadFiles(string filename, string path)
+        {
+            try
+            {
+                
+                var fs = new GridFSBucket(mongoDB.database);
+                var f = System.IO.File.OpenRead(path);
+                byte[] bytes = System.IO.File.ReadAllBytes(path);
+                var option = new GridFSUploadOptions
+                {
+
+
+                };
+                fs.UploadFromBytes(filename, bytes, option);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        [HttpGet("{id}")]
+        public bool DownLoadFiles(String id)
+        {
+            
+            try
+            {
+                var option = new GridFSUploadOptions
+                {
+
+
+                };
+                var fs = new GridFSBucket(mongoDB.database);
+                var fileDate = mongoDB.database.GetCollection<FileViewModel>("fs.files").Find(new BsonDocument()).ToList();
+                var f = fs.DownloadAsBytes(ObjectId.Parse(id));
+                var 
+                System.IO.File.WriteAllBytes(@"C:\Users\nicco\Downloads\prova7.pdf", f);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Socloo/SoclooAPI/Models/FileViewModel.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SoclooAPI.Models
+{
+    public class FileViewModel
+    {
+        public ObjectId Id { get; set; }
+        public int Length { get; set; }
+        public int ChunkSize { get; set; }
+        [BsonRepresentation(BsonType.DateTime)]
+        public DateTime UploadDate { get; set; }
+        public string Md5 { get; set; }
+        public string Filename { get; set; }
+    }
+}
