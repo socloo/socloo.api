9910434d40cd77839da5c14457785775c822f1e2
Niccolò Andreas Armellini <niccolo.armellini@live.com>
2019-07-03 11:21:09 +0200

    fix

---
+++ Socloo/SoclooAPI/Models/Filter.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+
+namespace SoclooAPI.Models
+{
+    public class Filter
+    {
+        public MongoDBContext mongoDB;
+        public string RemoveBadWord(string Text)
+        {
+            mongoDB = new MongoDBContext();
+             List<FilterViewModel> it = mongoDB.database.GetCollection<FilterViewModel>("Italian Bad Words").Find(new BsonDocument()).ToList();
+            List<FilterViewModel> fr = mongoDB.database.GetCollection<FilterViewModel>("French Bad Words").Find(new BsonDocument()).ToList();
+            List<FilterViewModel> de = mongoDB.database.GetCollection<FilterViewModel>("German Bad Words").Find(new BsonDocument()).ToList();
+            List<FilterViewModel> es = mongoDB.database.GetCollection<FilterViewModel>("Spanish Bad Words").Find(new BsonDocument()).ToList();
+            List<FilterViewModel> en = mongoDB.database.GetCollection<FilterViewModel>("English Bad Words").Find(new BsonDocument()).ToList();
+            Text = Text.ToLower();
+            foreach (var word in it)
+            {
+                if (Text.ToLower().Contains(word.Text))
+                {
+                    string newValue = "";
+                    for(int i = 0; i < word.Text.Length; i++)
+                    {
+                        newValue = newValue + "*";
+                    }
+                    Text =Text.Replace(word.Text, newValue);
+                }
+            }
+            foreach (var word in fr)
+            {
+                if (Text.ToLower().Contains(word.Text))
+                {
+                    string newValue = "";
+                    for (int i = 0; i < word.Text.Length; i++)
+                    {
+                        newValue = newValue + "*";
+                    }
+                    Text = Text.Replace(word.Text, newValue);
+                }
+            }
+            foreach (var word in de)
+            {
+                if (Text.ToLower().Contains(word.Text))
+                {
+                    string newValue = "";
+                    for (int i = 0; i < word.Text.Length; i++)
+                    {
+                        newValue = newValue + "*";
+                    }
+                    Text = Text.Replace(word.Text, newValue);
+                }
+            }
+            foreach (var word in es)
+            {
+                if (Text.ToLower().Contains(word.Text))
+                {
+                    string newValue = "";
+                    for (int i = 0; i < word.Text.Length; i++)
+                    {
+                        newValue = newValue + "*";
+                    }
+                    Text = Text.Replace(word.Text, newValue);
+                }
+            }
+            foreach (var word in en)
+            {
+                if (Text.ToLower().Contains(word.Text))
+                {
+                    string newValue = "";
+                    for (int i = 0; i < word.Text.Length; i++)
+                    {
+                        newValue = newValue + "*";
+                    }
+                    Text = Text.Replace(word.Text, newValue);
+                }
+            }
+            return Text;
+        }
+    }
+}
---
+++ Socloo/SoclooAPI/Models/FilterViewModel.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SoclooAPI.Models
+{
+    public class FilterViewModel
+    {
+        public ObjectId id { get; set; }
+        public string Text { get; set; }
+    }
+}
