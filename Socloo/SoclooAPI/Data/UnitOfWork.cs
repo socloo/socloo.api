93ab773ff79f8680bc73799e3bebd5b23af792c9
Niccolò Andreas Armellini <niccolo.armellini@live.com>
2019-07-09 09:36:08 +0200

    commit uow repository

--- Socloo/SoclooAPI/Data/UnitOfWork.cs
+++ Socloo/SoclooAPI/Data/UnitOfWork.cs
@@ -23,7 +23,247 @@ namespace SoclooAPI.Data
 
             return (Repository<T>)_repository[typeof(T)];
         }
+        public AnswerRepository<T> AnswerRepository<T>() where T : ILocalizableEntity<ObjectId>
+        {
+            if (!_geoRepository.Keys.Contains(typeof(T)))
+            {
+                var obj = new AnswerRepository<T>(_context);
 
+                _geoRepository.Add(typeof(T), obj);
+            }
+
+            return (AnswerRepository<T>)_geoRepository[typeof(T)];
+        }
+        public AnswerMCRepository<T> AnswerMCRepository<T>() where T : ILocalizableEntity<ObjectId>
+        {
+            if (!_geoRepository.Keys.Contains(typeof(T)))
+            {
+                var obj = new AnswerMCRepository<T>(_context);
+
+                _geoRepository.Add(typeof(T), obj);
+            }
+
+            return (AnswerMCRepository<T>)_geoRepository[typeof(T)];
+        }
+        public AnswerSARepository<T> AnswerSARepository<T>() where T : ILocalizableEntity<ObjectId>
+        {
+            if (!_geoRepository.Keys.Contains(typeof(T)))
+            {
+                var obj = new AnswerSARepository<T>(_context);
+
+                _geoRepository.Add(typeof(T), obj);
+            }
+
+            return (AnswerSARepository<T>)_geoRepository[typeof(T)];
+        }
+        public AnswerTFRepository<T> AnswerTFRepository<T>() where T : ILocalizableEntity<ObjectId>
+        {
+            if (!_geoRepository.Keys.Contains(typeof(T)))
+            {
+                var obj = new AnswerTFRepository<T>(_context);
+
+                _geoRepository.Add(typeof(T), obj);
+            }
+
+            return (AnswerTFRepository<T>)_geoRepository[typeof(T)];
+        }
+        public AssignmentRepository<T> AssignmentRepository<T>() where T : ILocalizableEntity<ObjectId>
+        {
+            if (!_geoRepository.Keys.Contains(typeof(T)))
+            {
+                var obj = new AssignmentRepository<T>(_context);
+
+                _geoRepository.Add(typeof(T), obj);
+            }
+
+            return (AssignmentRepository<T>)_geoRepository[typeof(T)];
+        }
+        public CalendarRepository<T> CalendarRepository<T>() where T : ILocalizableEntity<ObjectId>
+        {
+            if (!_geoRepository.Keys.Contains(typeof(T)))
+            {
+                var obj = new CalendarRepository<T>(_context);
+
+                _geoRepository.Add(typeof(T), obj);
+            }
+
+            return (CalendarRepository<T>)_geoRepository[typeof(T)];
+        }
+        public ChatRepository<T> ChatRepository<T>() where T : ILocalizableEntity<ObjectId>
+        {
+            if (!_geoRepository.Keys.Contains(typeof(T)))
+            {
+                var obj = new ChatRepository<T>(_context);
+
+                _geoRepository.Add(typeof(T), obj);
+            }
+
+            return (ChatRepository<T>)_geoRepository[typeof(T)];
+        }
+        public CourseRepository<T> CourseRepository<T>() where T : ILocalizableEntity<ObjectId>
+        {
+            if (!_geoRepository.Keys.Contains(typeof(T)))
+            {
+                var obj = new CourseRepository<T>(_context);
+
+                _geoRepository.Add(typeof(T), obj);
+            }
+
+            return (CourseRepository<T>)_geoRepository[typeof(T)];
+        }
+        public DashboardRepository<T> DashboardRepository<T>() where T : ILocalizableEntity<ObjectId>
+        {
+            if (!_geoRepository.Keys.Contains(typeof(T)))
+            {
+                var obj = new DashboardRepository<T>(_context);
+
+                _geoRepository.Add(typeof(T), obj);
+            }
+
+            return (DashboardRepository<T>)_geoRepository[typeof(T)];
+        }
+        public DocumentRepository<T> DocumentRepository<T>() where T : ILocalizableEntity<ObjectId>
+        {
+            if (!_geoRepository.Keys.Contains(typeof(T)))
+            {
+                var obj = new DocumentRepository<T>(_context);
+
+                _geoRepository.Add(typeof(T), obj);
+            }
+
+            return (DocumentRepository<T>)_geoRepository[typeof(T)];
+        }
+        public GroupRepository<T> GroupRepository<T>() where T : ILocalizableEntity<ObjectId>
+        {
+            if (!_geoRepository.Keys.Contains(typeof(T)))
+            {
+                var obj = new GroupRepository<T>(_context);
+
+                _geoRepository.Add(typeof(T), obj);
+            }
+
+            return (GroupRepository<T>)_geoRepository[typeof(T)];
+        }
+        public MessageRepository<T> MessageRepository<T>() where T : ILocalizableEntity<ObjectId>
+        {
+            if (!_geoRepository.Keys.Contains(typeof(T)))
+            {
+                var obj = new MessageRepository<T>(_context);
+
+                _geoRepository.Add(typeof(T), obj);
+            }
+
+            return (MessageRepository<T>)_geoRepository[typeof(T)];
+        }
+        public OccurrenceRepository<T> OccurrenceRepository<T>() where T : ILocalizableEntity<ObjectId>
+        {
+            if (!_geoRepository.Keys.Contains(typeof(T)))
+            {
+                var obj = new OccurrenceRepository<T>(_context);
+
+                _geoRepository.Add(typeof(T), obj);
+            }
+
+            return (OccurrenceRepository<T>)_geoRepository[typeof(T)];
+        }
+        public PorfolioRepository<T> PorfolioRepository<T>() where T : ILocalizableEntity<ObjectId>
+        {
+            if (!_geoRepository.Keys.Contains(typeof(T)))
+            {
+                var obj = new PorfolioRepository<T>(_context);
+
+                _geoRepository.Add(typeof(T), obj);
+            }
+
+            return (PorfolioRepository<T>)_geoRepository[typeof(T)];
+        }
+        public  PostRepository<T> PostRepository<T>() where T : ILocalizableEntity<ObjectId>
+        {
+            if (!_geoRepository.Keys.Contains(typeof(T)))
+            {
+                var obj = new PostRepository<T>(_context);
+
+                _geoRepository.Add(typeof(T), obj);
+            }
+
+            return (PostRepository<T>)_geoRepository[typeof(T)];
+        }
+
+
+        public QuestionRepository<T> QuestionRepository<T>() where T : ILocalizableEntity<ObjectId>
+        {
+            if (!_geoRepository.Keys.Contains(typeof(T)))
+            {
+                var obj = new QuestionRepository<T>(_context);
+
+                _geoRepository.Add(typeof(T), obj);
+            }
+
+            return (QuestionRepository<T>)_geoRepository[typeof(T)];
+        }
+
+        public SchoolAdminRepository<T> SchoolAdminRepository<T>() where T : ILocalizableEntity<ObjectId>
+        {
+            if (!_geoRepository.Keys.Contains(typeof(T)))
+            {
+                var obj = new SchoolAdminRepository<T>(_context);
+
+                _geoRepository.Add(typeof(T), obj);
+            }
+
+            return (SchoolAdminRepository<T>)_geoRepository[typeof(T)];
+        }
+
+
+
+        public StudentRepository<T> StudentRepository<T>() where T : ILocalizableEntity<ObjectId>
+        {
+            if (!_geoRepository.Keys.Contains(typeof(T)))
+            {
+                var obj = new StudentRepository<T>(_context);
+
+                _geoRepository.Add(typeof(T), obj);
+            }
+
+            return (StudentRepository<T>)_geoRepository[typeof(T)];
+        }
+
+
+        public SuperAdminRepository<T> SuperAdminRepository<T>() where T : ILocalizableEntity<ObjectId>
+        {
+            if (!_geoRepository.Keys.Contains(typeof(T)))
+            {
+                var obj = new SuperAdminRepository<T>(_context);
+
+                _geoRepository.Add(typeof(T), obj);
+            }
+
+            return (SuperAdminRepository<T>)_geoRepository[typeof(T)];
+        }
+
+        public TeacherRepository<T> TeacherRepository<T>() where T : ILocalizableEntity<ObjectId>
+        {
+            if (!_geoRepository.Keys.Contains(typeof(T)))
+            {
+                var obj = new TeacherRepository<T>(_context);
+
+                _geoRepository.Add(typeof(T), obj);
+            }
+
+            return (TeacherRepository<T>)_geoRepository[typeof(T)];
+        }
+
+        public TestRepository<T> TestRepository<T>() where T : ILocalizableEntity<ObjectId>
+        {
+            if (!_geoRepository.Keys.Contains(typeof(T)))
+            {
+                var obj = new TestRepository<T>(_context);
+
+                _geoRepository.Add(typeof(T), obj);
+            }
+
+            return (TestRepository<T>)_geoRepository[typeof(T)];
+        }
         public UserRepository<T> UserRepository<T>() where T : ILocalizableEntity<ObjectId>
         {
             if (!_geoRepository.Keys.Contains(typeof(T)))
@@ -35,7 +275,7 @@ namespace SoclooAPI.Data
 
             return (UserRepository<T>)_geoRepository[typeof(T)];
         }
-
+ 
         public UnitOfWork(DataContext context)
         {
             _repository = new Dictionary<Type, object>();
